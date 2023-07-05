ea10ca9e8e2ac44cfb58c0e1bb4f33d80a6e8d7c
Rolol <118076343+roimet@users.noreply.github.com>
2023-07-06 00:33:47 +0200

    Add dynamic resolution slider and setting buttons
    - Implement a function to clear and render the grid.
    - Add setting-related buttons and a resolution slider to control the grid size.
    - Dynamically update the grid and resolution text when the slider value changes.
    - Restructure the body in the html, placing the settings and sheet next to each
    other with the h2 on top.
    - Center the "total" class, the h2 and temporarily adjust the settings position.

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 const sheet = document.querySelector(".sheet");
+const slider = document.getElementById("resSlider");
+const resolutionText = document.querySelector(".Resolution");
 
-const res = [];
+console.log(resolutionText)
+renderCanvas(slider.value);
 
-const sheetSize = 500;
-const resolution = 32;
-const eachCube = sheetSize/resolution;
+slider.oninput = function() {
+    renderCanvas(this.value);
+    resolutionText.textContent = `Resolution - ${this.value}x${this.value}`;
+}
 
-for(i = 0; i < resolution; i++) {
-    res[i] = [];
-    for(j = 0; j < resolution; j++) {
-        const div = document.createElement('div');
+function renderCanvas(resolution) {
+    while (sheet.lastChild) {
+        sheet.removeChild(sheet.lastChild);
+    }
+    const sheetSize = 500;
+    const res = [];
+    const eachCube = sheetSize/resolution;
+    for(i = 0; i < resolution; i++) {
+        res[i] = [];
+        for(j = 0; j < resolution; j++) {
+            const div = document.createElement('div');
 
-        div.setAttribute('style', 
-        `box-sizing: border-box; width: ${eachCube}px; height: ${eachCube}px; border: 1px solid black`);
+            div.setAttribute('style', 
+            `box-sizing: border-box; width: ${eachCube}px; height: ${eachCube}px; border: 1px solid black`);
 
-        res[i][j] = div;
-        sheet.appendChild(res[i][j]);
+            res[i][j] = div;
+            sheet.appendChild(res[i][j]);
+        }
     }
 }
 
+//drawing part
 const divs = document.querySelectorAll(`.sheet div`);
-
 let isMouseDown = false;
 
 document.addEventListener('mousedown', function(e) {
