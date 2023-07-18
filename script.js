dfa3fbe7e34df19f0cedb305c501dbf8dd56b231
Rolol <118076343+roimet@users.noreply.github.com>
2023-07-18 02:36:57 +0200

    Implement rainbow mode and button styling
    - Introduce getRandomColor function for rainbow mode.
    - Add event listener to rainbow button to switch boolean variable isRainbow.
    - Update color on mousedown event and track lastDiv.
    - Change color only when entering a new div in mousemove event.
    - Apply/remove "active" class to setting buttons to toggle grey background.
    - Remove "active" class from toggle grid on resolution slider change.
    - Enhance grid visibility by changing grid color to black.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ renderCanvas(slider.value);
 slider.oninput = function() {
     renderCanvas(this.value);
     resolutionText.textContent = `Resolution - ${this.value}x${this.value}`;
+    toggleGrid.classList.remove("active");
 }
 
 function renderCanvas(resolution) {
@@ -22,7 +23,7 @@ function renderCanvas(resolution) {
             const div = document.createElement('div');
 
             div.setAttribute('style', 
-            `box-sizing: border-box; width: ${eachCube}px; height: ${eachCube}px; border: 1px solid grey`);
+            `box-sizing: border-box; width: ${eachCube}px; height: ${eachCube}px; border: 1px solid black`);
 
             res[i][j] = div;
             sheet.appendChild(res[i][j]);
@@ -30,17 +31,31 @@ function renderCanvas(resolution) {
     }
 }
 
+function getRandomColor() {
+    const r = (Math.round(Math.random()*255));
+    const g = (Math.round(Math.random()*255));
+    const b = (Math.round(Math.random()*255));
+    return `rgb(${r},${g},${b})`;
+}
+
 //drawing part
 const divs = document.querySelectorAll(`.sheet div`);
 let isMouseDown = false;
 let currentColor = "red";
+let lastDiv = null;
 
 document.addEventListener('mousedown', function(e) {
     const target = e.target;
     if(target.matches('.sheet div')) {
         e.preventDefault();
         isMouseDown = true;
-        target.style.backgroundColor = currentColor;
+        if(isRainbowOn) {
+            target.style.backgroundColor = getRandomColor();
+            lastDiv = target;
+        } else {
+            target.style.backgroundColor = currentColor;
+        }
+        
     }
 });
 
@@ -48,7 +63,14 @@ document.addEventListener('mousemove', function(e) {
     if(isMouseDown) {
         const target = e.target;
         if(target.matches('.sheet div')) {
-            target.style.backgroundColor = currentColor;
+            if(isRainbowOn) {
+                if(target !== lastDiv) {
+                    target.style.backgroundColor = getRandomColor(); 
+                    lastDiv = target;
+                }
+            } else {
+                target.style.backgroundColor = currentColor;
+            }
         }
     }
 });
@@ -66,13 +88,14 @@ const toggleGrid = document.querySelector(".toggleGrid");
 toggleGrid.addEventListener('click', function() {
     const childDivs = document.querySelectorAll(".sheet div");
     childDivs.forEach(div => {
-        if(div.style.border === "1px solid grey") {
+        if(div.style.border === "1px solid black") {
             div.style.border = "0px";
+            toggleGrid.classList.add("active");
         } else {
-            div.style.border = "1px solid grey";
+            div.style.border = "1px solid black";
+            toggleGrid.classList.remove("active");
         }
     });
-    
 });
 
 const eraser = document.querySelector(".eraser");
@@ -83,8 +106,24 @@ eraser.addEventListener('click', function() {
         isEraserOn = true;
         previousColor = currentColor;
         currentColor = "white";
+        eraser.classList.add("active");
     } else {
         isEraserOn = false;
         currentColor = previousColor;
+        eraser.classList.remove("active");
+    }
+});
+
+const rainbow = document.querySelector(".rainbow");
+let isRainbowOn = false;
+
+rainbow.addEventListener('click', function() {
+    if(!isRainbowOn) {
+        isRainbowOn = true;        
+        rainbow.classList.add("active");
+    } else {
+        isRainbowOn = false;
+        currentColor = "red";
+        rainbow.classList.remove("active");
     }
 });
